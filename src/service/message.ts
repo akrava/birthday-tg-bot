1496b89c6ac799a7c53216601d28f22b9bfdbe03
Arkadiy <arkadiy@ex.ua>
2020-08-23 01:00:19 +0300

    fix greeting message

--- src/service/message.ts
+++ src/service/message.ts
@@ -2,6 +2,6 @@ export function greetings(name: string, yearOld: number) {
     return `Happy birthday, dear ${name}!\n`
         + `Wishing you many happiness for the coming years ahead.\n`
         + `Congratulations on your ${yearOld}th birthday. `
-        + (yearOld >= 20 ? "Oh no, so old..." : "")
+        + (yearOld == 20 ? "Oh no, so old..." : "")
     ;
 }
