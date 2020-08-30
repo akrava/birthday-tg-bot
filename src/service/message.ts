f40cfcab26c2dd0d5071f4ed486f2c91a893e35e
Arkadiy <arkadiy@ex.ua>
2020-08-31 00:03:55 +0300

    fix message

--- src/service/message.ts
+++ src/service/message.ts
@@ -5,7 +5,7 @@ export async function greetings(name: string, yearOld: number) {
     return `Happy birthday, dear ${name}!\n`
         + `Wishing you many happiness for the coming years ahead.\n`
         + `Congratulations on your ${yearOld}th birthday. `
-        + (yearOld == 20 ? "Oh no, so old..." : "") +
+        + (yearOld == 20 ? "Oh no, so old..." : "")
         + (await getRandomText())
     ;
 }
